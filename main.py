2b528b83a34488ab1c94ed665cb7206297f814db
whdnjsdyd111 <66655578+whdnjsdyd111@users.noreply.github.com>
2021-07-25 22:13:43 +0900

    histogram

--- main.py
+++ main.py
@@ -1,25 +1,13 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+rg = np.random.default_rng(1)
 
-def mandelbrot(h, w, maxit=20, r=2):
-    x = np.linspace(-2.5, 1.5, 4*h + 1)
-    y = np.linspace(-1.5, 1.5, 3*w + 1)
-    a, b = np.meshgrid(x, y)
-    c = a + b * 1j
-    z = np.zeros_like(c)
-    divTime = maxit + np.zeros(z.shape, dtype=int)
+mu, sigma = 2, 0.5
+v = rg.normal(mu, sigma, 10000)
 
-    for i in range(maxit):
-        z = z ** 2 + c
-        diverge = abs(z) > r  # who is diverging
-        div_now = diverge & (divTime == maxit)  # who is diverging now
-        divTime[div_now] = i  # note when
-        z[diverge] = r  # avoid diverging too much
+plt.hist(v, bins=50, density=True)
 
-    return divTime
-
-
-plt.imshow(mandelbrot(400, 400))
+(n, bins) = np.histogram(v, bins=50, density=True)
+plt.plot(.5 * (bins[1:] + bins[:-1]), n)
 plt.show()
-
