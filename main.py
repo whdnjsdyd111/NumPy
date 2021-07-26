0a32b328235f2d61ead691a684c26ee9cae5172a
whdnjsdyd111 <66655578+whdnjsdyd111@users.noreply.github.com>
2021-07-26 23:51:07 +0900

    mask

--- main.py
+++ main.py
@@ -1,11 +1,30 @@
-from scipy import misc
-from numpy import linalg
+import numpy as np
+import os
 import matplotlib.pyplot as plt
 
-img = misc.face()
+filepath = os.getcwd()
+filename = os.path.join(filepath, "who_covid_19_sit_rep_time_series.csv")
 
-img_array = img / 255
-img_gray = img_array @ [0.2126, 0.7152, 0.0722]
+# skip_header 와 usecols 로 데이터 파일을 각 변수에 추가한다.
+# 첫 번째 행에서 3-7 열의 날짜만 읽는다.
+datas = np.genfromtxt(filename, dtype=np.unicode_, delimiter=",",
+                      max_rows=1, usecols=range(3, 17),
+                      encoding="utf-8-sig")
 
-plt.imshow(img_gray, cmap='gray')
+# 처음 두 컬럼으로 부터 위치 이름을 읽는다.
+# 처음 7 행은 생략한다.
+locations = np.genfromtxt(filename, dtype=np.unicode_, delimiter=",",
+                          skip_header=7, usecols=(0, 1),
+                          encoding="utf-8-sig")
+
+# 처음 14일 동안의 숫자 데이터를 읽는다.
+nbcases = np.genfromtxt(filename, dtype=np.int_, delimiter=",",
+                        skip_header=7, usecols=range(3, 17),
+                        encoding="utf-8-sig")
+
+selected_dates = [0, 3, 11, 13]
+
+plt.plot(datas, nbcases.T, '--')
+plt.xticks(selected_dates, datas[selected_dates])
+plt.title("COVID-19 cumulative cases")
 plt.show()
