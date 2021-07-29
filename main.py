42491df96b1adc905abec547d4458bbffd36651e
whdnjsdyd111 <66655578+whdnjsdyd111@users.noreply.github.com>
2021-07-30 01:06:49 +0900

    deap learning 1

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import requests
 import os
 import gzip
 import numpy as np
+import matplotlib.pylab as plt
 
 data_sources = {
     "training_images": "train-images-idx3-ubyte.gz",   # 60,000 training images.
@@ -20,7 +21,7 @@ for fname in data_sources.values():
     if not os.path.exists(fpath):
         print("Downloading file: " + fname)
         resp = requests.get(base_url + fname, stream=True)
-        resp.raise_for_status() # 다운로드가 성공했는지 확인
+        resp.raise_for_status()  # 다운로드가 성공했는지 확인
         with open(fpath, "wb") as fh:
             for chunk in resp.iter_content(chunk_size=128):
                 fh.write(chunk)
@@ -35,3 +36,19 @@ for key in ("training_images", "test_images"):
 for key in ("training_labels", "test_labels"):
     with gzip.open(os.path.join(data_dir, data_sources[key]), 'rb') as mnist_file:
         mnist_dataset[key] = np.frombuffer(mnist_file.read(), np.uint8, offset=8)
+
+x_train, y_train, x_test, y_test = (mnist_dataset["training_images"],
+                                    mnist_dataset["training_labels"],
+                                    mnist_dataset["test_images"],
+                                    mnist_dataset["test_labels"])
+
+print('The shape of training images: {} and training labels: {}'.format(x_train.shape, y_train.shape))
+print('The shape of test images: {} and test labels: {}'.format(x_test.shape, y_test.shape))
+
+# 훈련셋으로부터의 60,000 번째 이미지 (인덱스 59,999)를
+# (784,) 를 (28, 28) 로 적합한 모양으로 만든다.
+mnist_image = x_train[59999, :].reshape(28, 28)
+# 검은색 배경을 사용하려면 색상 매핑을 그레이스케일로 설정한다.
+plt.imshow(mnist_image, cmap='gray')
+plt.show()
+
